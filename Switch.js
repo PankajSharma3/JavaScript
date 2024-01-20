13bb650ae7e207e2edced1bdd055fe72d71e953c
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-20 19:44:23 +0530

    Create Switch.js

---
+++ Switch.js
@@ -0,0 +1,13 @@
+let grade = 'B';
+let result;
+switch(grade){
+    case 'A':
+    result = "Grade A";
+    break;
+    case 'B':
+    result = "Grade B";
+    break;
+    case 'C':
+    result = "Grade C";
+}
+console.log(result);
