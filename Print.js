38bbfe636836765b0fb735085604269140224c4b
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-20 19:06:16 +0530

    Create Print.js

---
+++ Print.js
@@ -0,0 +1,8 @@
+//To access an HTML element, JavaScript can use the document.getElementById(id) method.
+document.getElementById("demo").innerHTML("Hello World");
+
+//For testing
+document.write("Hello World"); 
+
+//For debugging
+console.log("Hello World")
