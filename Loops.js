85ecf2cf28e4c9bc1bbee42fca80a37055745cfe
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-20 19:53:42 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,19 @@
+//For Loop
+for(let i=1;i<11;i++){
+    console.log(i*10);
+}
+
+//Do while loop
+let i=1;
+do{
+    console.log(i*10);
+    i++;
+}while(i<11);
+
+
+//while loop
+let i=1;
+while(i<11){
+    console.log(i*10);
+    i++;
+}
